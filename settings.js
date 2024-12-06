15cc4f643aed2dc881d8356ddd8d905a75f00c07
CYBER WHITE PODDA <119735296+CYBERWHITEPODDA@users.noreply.github.com>
2024-12-06 15:08:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=fzAWjKra#mm8BE_iWjrP50A3yVCb-N8snabrTMWSfJ4VHCIVwOZA': process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!* ඔබට අවශය ඕනෑම දෙයක් දැන ගැනීමට +94 763698137 nb ekata wp msg karanna" : process.env.ALIVE_MSG,
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"false" : process.env.AUTO_READ_STATUS,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "true" : process.env.AUTO_VOICE,
 AUTO_REPLY: process.env.AUTO_REPLY === undefined ? "true" : process.env.AUTO_REPLY,
